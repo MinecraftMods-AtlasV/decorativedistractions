f2ffc6f0c1abf473fc429ef10aef735af4646f2f
atlasv <atlasvpersonal@gmail.com>
2026-04-20 09:03:54 +0100

    Fixed stair model in inventory being broken

--- src/main/java/info/atlasv/decorative_distractions/tint/datagen/TintBlockStateProvider.java
+++ src/main/java/info/atlasv/decorative_distractions/tint/datagen/TintBlockStateProvider.java
@@ -6,6 +6,7 @@ import info.atlasv.decorative_distractions.tint.block.TintBlocks;
 import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.neoforged.neoforge.client.model.generators.BlockModelBuilder;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
@@ -90,12 +91,13 @@ public class TintBlockStateProvider extends BlockStateProvider {
     private void registerTintedStairs(TintEntry entry, String name, ResourceLocation texture) {
         String n = name + "_stairs";
 
-        BlockModelBuilder straight = buildTintedStairsStraight(n, texture);
-        BlockModelBuilder inner    = buildTintedStairsInner(n + "_inner", texture);
-        BlockModelBuilder outer    = buildTintedStairsOuter(n + "_outer", texture);
+        BlockModelBuilder straight  = buildTintedStairsStraight(n, texture);
+        BlockModelBuilder inner     = buildTintedStairsInner(n + "_inner", texture);
+        BlockModelBuilder outer     = buildTintedStairsOuter(n + "_outer", texture);
+        BlockModelBuilder inventory = buildTintedStairsInventory(n, texture);
 
         stairsBlock(entry.stairs.get(), straight, inner, outer);
-        simpleBlockItem(entry.stairs.get(), straight);
+        simpleBlockItem(entry.stairs.get(), inventory);
     }
 
 
@@ -182,6 +184,39 @@ public class TintBlockStateProvider extends BlockStateProvider {
                 .face(Direction.SOUTH).uvs(0, 0, 8, 8).texture("#side").tintindex(0).cullface(Direction.SOUTH).end()
                 .face(Direction.WEST).uvs(8, 0, 16, 8).texture("#side").tintindex(0).end()
                 .face(Direction.EAST).uvs(8, 0, 16, 8).texture("#side").tintindex(0).cullface(Direction.EAST).end()
+                .end();
+    }
+
+    private BlockModelBuilder buildTintedStairsInventory(String modelName, ResourceLocation texture) {
+        return models()
+                .withExistingParent("block/" + modelName, "minecraft:block/block")
+                .texture("particle", texture)
+                .texture("bottom", texture).texture("top", texture).texture("side", texture)
+                // lower slab portion
+                .element().from(0, 0, 0).to(16, 8, 16)
+                .face(Direction.DOWN).uvs(0, 0, 16, 16).texture("#bottom").tintindex(0).cullface(Direction.DOWN).end()
+                .face(Direction.UP).uvs(0, 0, 16, 16).texture("#top").tintindex(0).end()
+                .face(Direction.NORTH).uvs(0, 8, 16, 16).texture("#side").tintindex(0).cullface(Direction.NORTH).end()
+                .face(Direction.SOUTH).uvs(0, 8, 16, 16).texture("#side").tintindex(0).cullface(Direction.SOUTH).end()
+                .face(Direction.WEST).uvs(0, 8, 16, 16).texture("#side").tintindex(0).cullface(Direction.WEST).end()
+                .face(Direction.EAST).uvs(0, 8, 16, 16).texture("#side").tintindex(0).cullface(Direction.EAST).end()
+                .end()
+                // upper step
+                .element().from(8, 8, 0).to(16, 16, 16)
+                .face(Direction.DOWN).uvs(8, 0, 16, 16).texture("#top").tintindex(0).end()
+                .face(Direction.UP).uvs(8, 0, 16, 16).texture("#top").tintindex(0).cullface(Direction.UP).end()
+                .face(Direction.NORTH).uvs(0, 0, 8, 8).texture("#side").tintindex(0).cullface(Direction.NORTH).end()
+                .face(Direction.SOUTH).uvs(0, 0, 8, 8).texture("#side").tintindex(0).cullface(Direction.SOUTH).end()
+                .face(Direction.WEST).uvs(0, 0, 16, 8).texture("#side").tintindex(0).end()
+                .face(Direction.EAST).uvs(0, 0, 16, 8).texture("#side").tintindex(0).cullface(Direction.EAST).end()
+                .end()
+                .transforms()
+                .transform(ItemDisplayContext.GUI)
+                .rotation(30, 135, 0).scale(0.625f)
+                .end()
+                .transform(ItemDisplayContext.FIXED)
+                .rotation(0, 90, 0).scale(0.5f)
+                .end()
                 .end();
     }
 }
